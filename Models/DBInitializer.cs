0d36d169e61861c2dee40c3c4c881d89374297ae
Agustin Rojas <agusaar@gmail.com>
2024-05-27 18:40:59 -0300

    Agregar archivos de proyecto.

---
+++ Models/DBInitializer.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplication1.Models
+{
+    public class DBInitializer
+    {
+        public static void initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client { Email = "agusaar@gmail.com", FirstName="Agustin", LastName="Rojas", Password="123456"},
+                    new Client { Email = "martin@gmail.com", FirstName="Martin", LastName="Perez", Password="7890"},
+                    new Client { Email = "laura@gmail.com", FirstName="Laura", LastName="Alvarez", Password="admin1234"},
+                    new Client { Email = "javier@gmail.com", FirstName="Javier", LastName="Fernandez", Password="123456"},
+                };
+
+                context.Clients.AddRange(clients);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+//Agrego contexto de la BD
+builder.Services.AddDbContext<HomeBankingContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HomeBankingConexion")));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<HomeBankingContext>();
+        DBInitializer.initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ha ocurrido un error al enviar la información a la base de datos!");
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
